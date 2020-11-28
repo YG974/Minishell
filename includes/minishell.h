e8e38ed250bea59ce6409d98b682dacb1c730eba
ygeslin <ygeslin@pc-137.home>
2020-11-28 16:49:41 +0400

    makefile yann et debut main

---
+++ includes/minishell.h
@@ -0,0 +1,28 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <errno.h>
+# include <signal.h>
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
+# define BUFF_SIZE 4096
+
+typedef struct s_mini
+{
+	int		in;
+	int		out;
+}
+
+#endif
