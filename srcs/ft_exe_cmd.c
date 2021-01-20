88ec7cfe48af5bcf701c0badad1b4c71e3171b71
Yann GESLIN <ygeslin@e1r11p14.42.fr>
2021-01-20 14:20:10 +0100

    conditions si seulement des assignations, si builtin ou si autre fonction okay

--- srcs/ft_exe_cmd.c
+++ srcs/ft_exe_cmd.c
@@ -8,34 +8,80 @@ int		ft_exit_test(t_tok *token)
 	return (0);
 }
 
-int		ft_exe_tokens(t_cmdl *cmd)
+int		ft_is_builtin(char *str)
+{
+	if (!ft_strncmp(str, "echo", 4))
+		return (1);
+	if (!ft_strncmp(str, "cd", 2))
+		return (1);
+	if (!ft_strncmp(str, "pwd", 3))
+		return (1);
+	if (!ft_strncmp(str, "export", 6))
+		return (1);
+	if (!ft_strncmp(str, "unset", 5))
+		return (1);
+	if (!ft_strncmp(str, "env", 3))
+		return (1);
+	if (!ft_strncmp(str, "exit", 4))
+		return (1);
+	return (0);
+
+}
+
+int		parse_cmd_args(t_mini *s, t_cmdl *cmd)
+{
+	(void)s;
+	// (void)cmd;
+	ft_printf("parse_cmd_args\n");
+	return (cmd->ret);
+}
+
+int		exec_builtin(t_mini *s, t_cmdl *cmd)
+{
+	(void)s;
+	// (void)cmd;
+	ft_printf("exec buliltin\n");
+	return (cmd->ret);
+}
+
+int		ft_exe_tokens(t_mini *s, t_cmdl *cmd)
 {
 	int i;
-	char *cmd_str;
 
 	i = 1;
 	cmd->token = cmd->firsttoken;
-	cmd_str = ft_strdup("");
 	while (cmd->token)
 	{
     	printf("------- TOKEN #%d\n\tline : %s\n\tflag : %d\n-------\n\n", i, cmd->token->str, cmd->token->flag);
-		cmd_str = ft_strjoin_free_s1(cmd_str, "\n");
-		if (ft_strchr(cmd->token->str, '='))
-			cmd_str = ft_strjoin_free_s1(cmd_str, cmd->token->str);
-		if (!(ft_strchr(cmd->token->str, '=')) && cmd->token->flag != 2)
-		{
-			if (cmd_str)
-				free(cmd_str);
-			cmd_str = ft_strdup(cmd->token->str);
-		}
-		if (ft_exit_test(cmd->token))
-			return (WANT_EXIT);
-		if (cmd_str)
-			printf("%s\n", cmd_str);
+		if (ft_is_builtin(cmd->token->str))
+			cmd->ret = exec_builtin(s, cmd);
 		cmd->token = cmd->token->next;
 		i++;
 	}
     return (0);
+}
+
+int		apply_assignement(t_mini *s, t_cmdl *cmd)
+{
+	(void)s;
+	// (void)cmd;
+	ft_printf("apply assignement \n");
+	return (cmd->ret);
+
+}
+int		cmd_has_only_assignement(t_cmdl *cmd)
+{
+	int		cmd_flag;
+	
+	cmd_flag = 0;
+	cmd->token = cmd->firsttoken;
+	while (cmd->token)
+	{
+		if (!(ft_strchr(cmd->token->str, '=')) && cmd->token->flag != 2)
+			return (0);
+		cmd->token = cmd->token->next;
+	}
+	return (1);
 }
 /*
  * WARNING : the first argument is not always the command
@@ -50,7 +96,11 @@ void	ft_exe_cmd(t_mini *s, t_cmdl *cmd)
 
     ft_printf("=============>On est rentré dans la fonction d'EXECUTION COMMANDES\n");
 	cmd->ret = 0;
-	if ((cmd->ret = ft_exe_tokens(cmd)))
+	if (cmd_has_only_assignement(cmd))
+		cmd->ret = apply_assignement(s, cmd);
+	else 
+		parse_cmd_args(s, cmd);
+	if ((cmd->ret = ft_exe_tokens(s, cmd)))
 		error(s, cmd->ret);
     ft_printf("=============>On est sorti de la fonction d'EXECUTION COMMANDES\n");
 }
