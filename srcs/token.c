184e8ce00cc406dc3a5a8473f7334e5d72f1cac0
Yann GESLIN <ygeslin@e2r6p10.42.fr>
2021-02-17 17:19:21 +0100

    debut de reflexion sur l'ajout des token

--- srcs/token.c
+++ srcs/token.c
@@ -13,21 +13,28 @@
 #include "../libft/libft.h"
 #include "../includes/minishell.h"
 
-void	add_blank(t_mini *s, int j)
+void	add_blank(t_mini *s, int j, t_tok *tok)
 {
-	t_tok	*tok;
+	t_tok	*new;
 	
-	if (!(tok = ft_calloc(1, sizeof(t_cmdl))))
+	if (!(new = ft_calloc(1, sizeof(t_cmdl))))
 		error(s, ERR_CALLOC);
 	while (s->p.flag[s->i] == '5')
 		s->i++;
-	tok->str = ft_strdup(" ");
-	tok->flag = 5;
+	new->str = ft_strdup(" ");
+	new->flag = 5;
+	if (tok)
+	{
+		tok->next = new;
+		new->prev = tok;
+	}
+	
 }
 int		break_cmdline_into_token(t_mini *s)
 {
 	t_cmdl	*cmd;
 
+	cmd = init
 	if (!(cmd = ft_calloc(1, sizeof(t_cmdl))))
 		error(s, ERR_CALLOC);
 	cmd->firsttoken = tok;
@@ -36,11 +43,16 @@ int		break_cmdline_into_token(t_mini *s)
 	while (s->p.buf[s->i])
 	{
 		if (s->p.flag[s->i] == '5')
-			add_blank(s, s->i);
-
+			add_blank(s, s->i, tok);
+		else if (s->p.flag[s->i] == '1')
+			add_simple_quote(s, s->i, tok);
+		else if (s->p.flag[s->i] == '2')
+			add_double_quote(s, s->i, tok);
+		else if (s->p.flag[s->i] == '3')
+			add_backslash(s, s->i, tok);
+		else if (s->p.flag[s->i] == '3')
+			add_word(s, s->i, &tok);
 		s->i++;
-
 	}
-
 	return (1);
 }
