924a52483a519fcdf7934788ed75ca8a69ec789a
ygeslin <ygeslin@ygeslins-MacBook-Pro.local>
2021-02-17 23:38:07 +0100

    ok linkdes token etc

--- srcs/token.c
+++ srcs/token.c
@@ -13,46 +13,82 @@
 #include "../libft/libft.h"
 #include "../includes/minishell.h"
 
-void	add_blank(t_mini *s, int j, t_tok *tok)
+t_tok	*link_token(t_mini *s, t_tok *tok, t_tok *new)
+{
+	if (tok)
+	{
+		tok->next = new;
+		new->prev = tok;
+		s->currentcmdl->token = tok->next;
+	}
+	else
+		s->currentcmdl->firsttoken = new;
+	return (new);
+}
+
+t_tok	*add_blank(t_mini *s, int j, t_tok *tok)
 {
 	t_tok	*new;
 	
+	j = s->i;
 	if (!(new = ft_calloc(1, sizeof(t_cmdl))))
 		error(s, ERR_CALLOC);
 	while (s->p.flag[s->i] == '5')
 		s->i++;
 	new->str = ft_strdup(" ");
 	new->flag = 5;
-	if (tok)
-	{
-		tok->next = new;
-		new->prev = tok;
-	}
-	
+	new = link_token(s, tok, new);
+	return (new);
 }
+
+t_tok	*add_simple_quote(t_mini *s, int j, t_tok *tok)
+{
+	t_tok	*new;
+	
+	if (!(new = ft_calloc(1, sizeof(t_cmdl))))
+		error(s, ERR_CALLOC);
+	while (s->p.flag[s->i] == '1')
+		s->i++;
+	new->str = ft_strdup_size(s->p.str, s->i -1, j + 1);
+	new->flag = 1;
+	new = link_token(s, tok, new);
+	return (new);
+}
+
+void	print_token(t_tok *tok)
+{
+	int i = 0;
+	while (tok)
+	{
+		i++;
+		ft_printf("%d-tok:%s\nflag:%d\nn----\n", i, tok->str,tok->flag);
+		tok = tok->next;
+	}
+}
+
 int		break_cmdline_into_token(t_mini *s)
 {
 	t_cmdl	*cmd;
 
-	cmd = init
 	if (!(cmd = ft_calloc(1, sizeof(t_cmdl))))
 		error(s, ERR_CALLOC);
-	cmd->firsttoken = tok;
 	s->firstcmdl = cmd;
+	s->currentcmdl = cmd;
 	s->i = 0;
 	while (s->p.buf[s->i])
 	{
 		if (s->p.flag[s->i] == '5')
-			add_blank(s, s->i, tok);
+			cmd->token = add_blank(s, s->i, cmd->token);
 		else if (s->p.flag[s->i] == '1')
-			add_simple_quote(s, s->i, tok);
-		else if (s->p.flag[s->i] == '2')
-			add_double_quote(s, s->i, tok);
-		else if (s->p.flag[s->i] == '3')
-			add_backslash(s, s->i, tok);
-		else if (s->p.flag[s->i] == '3')
-			add_word(s, s->i, &tok);
+			cmd->token = add_simple_quote(s, s->i, cmd->token);
+		/*else if (s->p.flag[s->i] == '2')*/
+			/*cmd->token = add_double_quote(s, s->i, cmd->token);*/
+		/*else if (s->p.flag[s->i] == '3')*/
+			/*cmd->token = add_backslash(s, s->i, cmd->token);*/
+		/*else if (s->p.flag[s->i] == '3')*/
+			/*cmd->token = add_word(s, s->i, cmd->token);*/
 		s->i++;
 	}
+	print_token(cmd->firsttoken);
 	return (1);
 }
