9e920403adf1faacabe6b03f9ffefee4dac5e135
Paco COUREAU <pcoureau@e2r4p4.42.fr>
2021-02-19 17:16:33 +0100

    redir ready for new version of parsing

--- srcs/ft_redirection.c
+++ srcs/ft_redirection.c
@@ -78,3 +78,69 @@ int		ft_redirection(t_mini *s, t_cmdl *cmd)
 	}
 	return (0);
 }
+
+/* POUR NOUVELLE VERSION DE PARSING
+int		ft_change_fd2(t_mini *s, t_tok *tok, t_tok *tmp)
+{
+	if (s->std.out > 1)
+		close(s->std.out);
+	if (tok->flag == D_GREATER)
+		s->std.out = open(tmp->str, O_WRONLY | O_CREAT | O_APPEND, 00644);
+	else
+		s->std.out = open(tmp->str, O_WRONLY | O_CREAT | O_TRUNC, 00644);
+	if (s->std.out < 0)
+	{
+		s->std.out = 0;
+		return (1);
+	}
+	else
+		dup2(s->std.out, 1);
+	return (0);
+}
+
+int		ft_change_fd(t_mini *s, t_tok *tok)
+{
+	t_tok	*tmp;
+
+	tmp = tok->next;
+	while (tmp && tmp->flag != REDIR_FLAG)
+		tmp = tmp->next;
+	if (!tmp)
+		return (1);
+	if (tok->flag == S_GREATER || tok->flag == D_GREATER)
+		return (ft_change_fd2(s, tok, tmp));
+	if (tok->flag == S_LESS)
+	{
+		if (s->std.in > 0)
+			close(s->std.in);
+		s->std.in = open(tmp->str, O_RDONLY);
+		if (s->std.in < 0)
+		{
+			s->std.in = 0;
+			return (1);
+		}
+		else
+			dup2(s->std.in, 0);
+	}
+	return (0);
+}
+
+int		ft_redirection(t_mini *s, t_cmdl *cmd)
+{
+	t_tok	*tmp;
+
+	tmp = cmd->firsttoken;
+	while (tmp && tmp->flag != S_PIPE)
+	{
+		if (tmp->flag >= 11 && tmp->flag <= 13)
+			if (ft_change_fd(s, tmp))
+			{
+				while (tmp && tmp->flag != REDIR_ARG)
+					tmp = tmp->next;
+				return (ft_redir_error(tmp->str));
+			}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+*/
