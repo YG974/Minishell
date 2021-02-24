f3677657c5a34408a5ef8f79c18ebdd7eb2fe7b7
Yann GESLIN <ygeslin@e2r5p8.42.fr>
2021-02-24 11:41:25 +0100

    try to fix dollar

--- srcs/break_into_token.c
+++ srcs/break_into_token.c
@@ -84,7 +84,7 @@ int		break_cmdline_into_token(t_mini *s)
 			cmd->token = add_backslash(s, s->i, cmd->token);
 		else if (s->p.flag[s->i] == '6')
 			cmd->token = add_meta(s, s->i, cmd->token);
-		else if (s->p.flag[s->i] == '7')
+		else if (s->p.flag[s->i] == '7' || s->p.flag[s->i] == '4')
 			cmd->token = add_word(s, s->i, cmd->token);
 		else if (s->p.flag[s->i] == '8')
 			cmd->token = add_newline(s, s->i, cmd->token);
