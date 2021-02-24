c264e9c23b8def9ca5cefee596e62262c190d3f3
ygeslin <ygeslin@student.42.fr>
2021-02-24 20:52:35 +0100

    del try to fix leaks dans split dollar parceque probleme apresss

--- srcs/env.c
+++ srcs/env.c
@@ -88,10 +88,10 @@ char	**put_env_in_tab(t_mini *s)
 	{
 		if (env->value)
 		{
-			env_str = ft_strjoin_free_s1(env_str, (env->name));
-			env_str = ft_strjoin_free_s1(env_str, "=");
-			env_str = ft_strjoin_free_s1(env_str, (env->value));
-			env_str = ft_strjoin_free_s1(env_str, "\n");
+			env_str = ft_strjoin_free_s1(ft_strdup(env_str), (env->name));
+			env_str = ft_strjoin_free_s1(ft_strdup(env_str), "=");
+			env_str = ft_strjoin_free_s1(ft_strdup(env_str), (env->value));
+			env_str = ft_strjoin_free_s1(ft_strdup(env_str), "\n");
 		}
 		env = env->next;
 	}
