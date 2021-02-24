5190c58733ae5e1f8c3fefb3f9170059447f0386
ygeslin <ygeslin@student.42.fr>
2021-02-24 19:41:09 +0100

    del try to fix leaks dans split dollar parceque probleme apresss

--- srcs/alloc_token.c
+++ srcs/alloc_token.c
@@ -53,6 +53,8 @@ t_tok	*add_word(t_mini *s, int j, t_tok *tok)
 	if (s->p.flag[s->i - 1] == '7')
 		new->flag = T_WORD;
 	/*new->flag = T_WORD;*/
+	if (new->str)
+		free(new->str);
 	new->str = ft_strdup_size(s->p.str, s->i, j);
 	new = link_token(s, tok, new);
 	return (new);
