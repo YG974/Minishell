a57131d481bfbff4637b263db648ac64abb14eb1
ygeslin <ygeslin@student.42.fr>
2021-02-25 11:08:14 +0100

    replace size of calloc, j'avais mis size of cmdl au lieu de putain de tok

--- srcs/alloc_token.c
+++ srcs/alloc_token.c
@@ -21,7 +21,7 @@ t_tok	*add_meta(t_mini *s, int j, t_tok *tok)
 {
 	t_tok	*new;
 
-	if (!(new = ft_calloc(1, sizeof(t_cmdl))))
+	if (!(new = ft_calloc(1, sizeof(t_tok))))
 		error(s, ERR_CALLOC);
 	if (s->p.str[s->i] == '>' && s->p.str[s->i + 1] == '|')
 		s->i++;
@@ -42,7 +42,7 @@ t_tok	*add_word(t_mini *s, int j, t_tok *tok)
 {
 	t_tok	*new;
 
-	if (!(new = ft_calloc(1, sizeof(t_cmdl))))
+	if (!(new = ft_calloc(1, sizeof(t_tok))))
 		error(s, ERR_CALLOC);
 	while (s->p.flag[s->i] == '7')
 		s->i++;
@@ -69,7 +69,7 @@ t_tok	*add_newline(t_mini *s, int j, t_tok *tok)
 	t_tok	*new;
 
 	(void)j;
-	if (!(new = ft_calloc(1, sizeof(t_cmdl))))
+	if (!(new = ft_calloc(1, sizeof(t_tok))))
 		error(s, ERR_CALLOC);
 	new->str = ft_strdup("newline");
 	new->flag = NEWLINE;
@@ -87,7 +87,7 @@ t_tok	*add_blank(t_mini *s, int j, t_tok *tok)
 	t_tok	*new;
 
 	(void)j;
-	if (!(new = ft_calloc(1, sizeof(t_cmdl))))
+	if (!(new = ft_calloc(1, sizeof(t_tok))))
 		error(s, ERR_CALLOC);
 	while (s->p.flag[s->i] == '5')
 		s->i++;
