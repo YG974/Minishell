1174c4342cea6445abe57dc783c46a227c8e14cd
ygeslin <ygeslin@student.42.fr>
2021-03-02 21:57:32 +0100

    segfault si echo /Users/ygeslin/42/Minishell/Users/ygeslin/42/Minishell et double free

--- srcs/handle_dollar.c
+++ srcs/handle_dollar.c
@@ -95,6 +95,7 @@ void	expand_dollars(t_mini *s, t_cmdl *cmd, int i, int j)
 			cmd->token->str = tmp;
 			cmd->token = split_dollar_token(s, cmd->token,
 					cmd->token->prev, cmd->token->next);
+			/*free(tmp);*/
 		}
 		cmd->token = cmd->token->next;
 	}
